2599172f227617731e4eb48a647745b5929f6d0b
Gustavo pereira da silva <gutzs1212@gmail.com>
2024-07-10 21:20:16 -0300

    first commit

--- app/src/components/carroselAvaliacoes.jsx
+++ app/src/components/carroselAvaliacoes.jsx
@@ -1,59 +1,52 @@
 import { bd } from "../bd";
 import Carousel from "react-grid-carousel";
 import "../style/styleCarroselAvaliacoes.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { useState } from "react";
 import imgAvaliacao from '../img/Vector.svg';
 import imgEmoji from '../img/emoji.svg';
 export default function CarroselAvaliacoes() {
   const [bancoAvaliacoes, setBancoAvaliacoes] = useState(1)
-  const [visibleProducts, setVisibleProducts] = useState(3);
-
+  // const [visibleProducts, setVisibleProducts] = useState(3);
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true,
+  };
   return (
     <section className="container-carrosel">
       <div className="title-avaliacoes">
         <h1>AVALIAÇOES DOS NOSSOS CLIENTES</h1>
       </div>
-      <div className="container-carrosel-avaliacoes">
-        <Carousel cols={1} loop >
-          <Carousel.Item>
-            <div className="container-cards">
-            {bd.slice(0, visibleProducts).map((e) => (
-            <div className="container-card">
-              <div className="ativo"></div>
-              <div className="container-img-nome">
-                <div
-                  style={{ backgroundImage: `url(${bd[bancoAvaliacoes].img})` }}
-                  className="img-avaliacao">
-                </div>
-                <div className="nome">
-                  <h2>ISABELA</h2>
-                </div>
-              </div>
-              <div className="text">
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam facilis maiores corrupti, harum voluptatum sunt, esse magnam ducimus totam
-                </p>
-              </div>
-              <div className="container-av-em">
-                <div className="avaliacao">
-                  <img src={imgAvaliacao} alt="" />
-                  <img src={imgAvaliacao} alt="" />
-                  <img src={imgAvaliacao} alt="" />
-                  <img src={imgAvaliacao} alt="" />
-                  <img src={imgAvaliacao} alt="" />
-                </div>
-                <div className="emoji">
-                <img src={imgEmoji} alt="" />
-                </div>
-              </div>
-              
-            </div>
-        ))}
-            </div>
-          
-          </Carousel.Item>
-        </Carousel>
-      </div>
+
+      <div className="slider-container">
+      <Slider {...settings}>
+        <div className="container-card">
+            
+        </div>
+        <div className="container-card">
+          <h3>2</h3>
+        </div>
+        <div className="container-card">
+          <h3>3</h3>
+        </div>
+        <div className="container-card">
+          <h3>4</h3>
+        </div>
+        <div className="container-card">
+          <h3>5</h3>
+        </div>
+        <div className="container-card">
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
     </section>
   );
 }
--- app/vite.config.js
+++ app/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:"/buzzCalcados"
+export default defineConfig({ 
+   base:"/app",
+   plugins: [react()]
+
 })
